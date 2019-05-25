31dfbafdaef4b5b11b8d8fc2b8cd19a9b0658d02
rhye <gacillos.rhye@gmail.com>
2019-05-26 03:16:09 +0800

    - SportsStore Angular Project

---
+++ src/app/store/store.component.ts
@@ -0,0 +1,21 @@
+import {Component} from '@angular/core';
+import {ProductRepository} from '../model/product.repository';
+import {Product} from '../model/Product';
+
+@Component({
+  selector: 'store',
+  templateUrl: 'store.component.html'
+})
+
+export class StoreComponent {
+
+  constructor(private repository: ProductRepository) {}
+
+  get products(): Product[] {
+    return this.repository.getProducts();
+  }
+
+  get categories(): string[] {
+    return this.repository.getCategories();
+  }
+}
