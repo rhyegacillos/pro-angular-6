917f7ca1085ab9515587875ba67e3d05d4f93181
rhye <gacillos.rhye@gmail.com>
2019-08-24 23:52:47 +0800

    - providing additional context data

--- src/app/product/directives/iterator.directive.ts
+++ src/app/product/directives/iterator.directive.ts
@@ -13,11 +13,21 @@ export class PaIteratorDirective implements OnInit{
   ngOnInit(): void {
     this.container.clear();
     for(let i = 0; i < this.datasource.length; i++) {
-      this.container.createEmbeddedView(this.template, new PaIteratorContext(this.datasource[i]));
+      this.container.createEmbeddedView(this.template, new PaIteratorContext(this.datasource[i], i, this.datasource.length));
     }
   }
 }
 
 class PaIteratorContext {
-  constructor(public $implicit: any) {}
+  odd: boolean;
+  even: boolean;
+  first: boolean;
+  last: boolean;
+
+  constructor(public $implicit: any, public index: number, total: number) {
+    this.odd = index % 2 === 1;
+    this.even = !this.odd;
+    this.first = index === 0;
+    this.last = index === total - 1;
+  }
 }
