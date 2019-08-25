eab1e1b9e7369d3564e3d5e68b623d14e04e7455
rhye <gacillos.rhye@gmail.com>
2019-08-25 18:48:35 +0800

    - Using Data Bindings in Component Templates

--- src/app/product/product-form/product-form.component.ts
+++ src/app/product/product-form/product-form.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductFormComponent implements OnInit {
 
+  model = 'This is the model';
+
   constructor() { }
 
   ngOnInit() {
