9a443e355caff91162d4f68ec4661776200b7bef
rhye <gacillos.rhye@gmail.com>
2019-08-25 22:28:43 +0800

    - Chapter 17 - Understanding Components - Using the Shadow DOM CSS Selectors

--- src/app/product/product-table/product-table.component.ts
+++ src/app/product/product-table/product-table.component.ts
@@ -1,22 +1,28 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit, QueryList, ViewChildren} from '@angular/core';
 import {Model} from '../../repository.model';
 import {Product} from '../product.model';
+import {PaCellColor} from '../directives/cellColor.directive';
 
 @Component({
   selector: 'app-product-table',
   templateUrl: './product-table.component.html',
   styleUrls: ['./product-table.component.css']
 })
-export class ProductTableComponent implements OnInit {
+export class ProductTableComponent implements AfterViewInit {
 
   @Input('model')
   dataModel: Model;
 
   showTable = true;
 
-  constructor() { }
+  @ViewChildren(PaCellColor)
+  viewChildren: QueryList<PaCellColor>;
 
-  ngOnInit() {
+  ngAfterViewInit(): void {
+    this.viewChildren.changes.subscribe(() => {
+      this.updateViewChildren();
+    });
+    this.updateViewChildren();
   }
 
   getProduct(key: number): Product {
@@ -29,6 +35,14 @@ export class ProductTableComponent implements OnInit {
 
   deleteProduct(key: number) {
     this.dataModel.deleteProduct(key);
+  }
+
+  private updateViewChildren() {
+    setTimeout(() => {
+      this.viewChildren.forEach((child, index) => {
+        child.setColor(!!(index % 2));
+      });
+    }, 0);
   }
 
 }
