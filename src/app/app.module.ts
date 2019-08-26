514260251681160897a99ae9c421ce0ac169f1dd
rayan <rayan.gacillos@searchoptmedia.com>
2019-08-27 01:38:53 +0800

    - Chapter 22 - Creating the Example Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ModelModule} from './model/model.module';
+import {CoreModule} from './core/core.module';
+import {MessageModule} from './messages/message/message.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule, ModelModule, CoreModule, MessageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
