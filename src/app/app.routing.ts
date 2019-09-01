109e39180af6f8f52587a208f10f640d8b1e0197
Rhye Gacillos <gacillos.rhye@gmail.com>
2019-09-02 03:54:09 +0800

    - Using Multiple Route Parameters

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,8 +3,8 @@ import {FormComponent} from "./core/form/form.component";
 import {TableComponent} from "./core/table/table.component";
 
 const routes: Routes = [
-  {path: 'form/edit', component: FormComponent},
+  {path: 'form/:mode/:id', component: FormComponent},
   {path: 'form/create', component: FormComponent},
   {path: '', component: TableComponent}
-]
+];
 export const routing = RouterModule.forRoot(routes);
--- src/app/core/form/form.component.ts
+++ src/app/core/form/form.component.ts
@@ -1,10 +1,8 @@
-import {Component, Inject} from '@angular/core';
+import {Component} from '@angular/core';
 import {Product} from '../../model/product.model';
 import {Model} from '../../model/repository.model';
-import {MODES, SHARED_STATE, SharedState} from '../sharedState.model';
 import {NgForm} from '@angular/forms';
-import {Observable} from 'rxjs';
-import {distinctUntilChanged, filter, map} from 'rxjs/operators';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-form',
@@ -14,19 +12,13 @@ import {distinctUntilChanged, filter, map} from 'rxjs/operators';
 export class FormComponent {
   product: Product = new Product();
   editing = false;
-  // lastId: number;
 
-  constructor(private model: Model, @Inject(SHARED_STATE) private stateEvents: Observable<SharedState>) {
-    stateEvents
-      // .pipe(distinctUntilChanged((firstState, secondState) =>
-      //   firstState.mode === secondState.mode && firstState.id === secondState.id))
-      .subscribe((update) => {
-        this.product = new Product();
-        if (update.id !== undefined) {
-          Object.assign(this.product, this.model.getProduct(update.id));
-        }
-        this.editing = update.mode === MODES.EDIT;
-    });
+  constructor(private model: Model, activeRoute: ActivatedRoute) {
+   this.editing = activeRoute.snapshot.params['mode'] === 'edit';
+   const id = +activeRoute.snapshot.params['id'];
+   if (id != null) {
+     Object.assign(this.product, model.getProduct(id) || new Product())
+   }
   }
 
   submitForm(form: NgForm) {
