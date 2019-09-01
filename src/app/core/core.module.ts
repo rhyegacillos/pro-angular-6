2401619659f469bb516a76dff2d2c183aeba0580
Rhye Gacillos <gacillos.rhye@gmail.com>
2019-09-02 02:59:35 +0800

    - Getting Star ted with Routing

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -18,11 +18,11 @@ import {Message} from '../messages/message.model';
   providers: [{provide: SHARED_STATE,
     deps: [MessageService, Model],
     useFactory: (messageService, model) => {
-      const subject = new Subject<SharedState>();
-      subject.subscribe(m => messageService.reportMessage(
-        new Message(MODES[m.mode] + (m.id !== undefined ? ` ${model.getProduct(m.id).name}` : '')))
-      );
-      return subject;
+      return  new Subject<SharedState>();
+      // subject.subscribe(m => messageService.reportMessage(
+      //   new Message(MODES[m.mode] + (m.id !== undefined ? ` ${model.getProduct(m.id).name}` : '')))
+      // );
+      // return subject;
     }
   }]
 })
