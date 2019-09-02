f47101e4ab2ef2fc6210069310aa46dcad4b1206
Rhye Gacillos <gacillos.rhye@gmail.com>
2019-09-02 11:21:34 +0800

    - Receiving Navigation Events

--- src/app/messages/message.module.ts
+++ src/app/messages/message.module.ts
@@ -3,9 +3,10 @@ import {BrowserModule} from '@angular/platform-browser';
 import {MessageComponent} from './message/message.component';
 import {MessageService} from './message.service';
 import {MessageErrorHandler} from "./error-handler";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule],
   declarations: [MessageComponent],
   exports: [MessageComponent],
   providers: [MessageService, {provide: ErrorHandler, useClass: MessageErrorHandler}]
--- src/app/messages/message/message.component.ts
+++ src/app/messages/message/message.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {Message} from '../message.model';
 import {MessageService} from '../message.service';
+import {NavigationCancel, NavigationEnd, Router} from "@angular/router";
+import {filter} from "rxjs/operators";
 
 @Component({
   selector: 'app-message',
@@ -11,8 +13,10 @@ export class MessageComponent {
 
   lastMessage: Message;
 
-  constructor(messageService: MessageService) {
+  constructor(messageService: MessageService, router: Router) {
     messageService.messages.subscribe(m => this.lastMessage = m);
+    router.events
+      .pipe(filter(e => e instanceof NavigationEnd || e instanceof NavigationCancel))
+      .subscribe(e => {this.lastMessage = null;});
   }
-
 }
